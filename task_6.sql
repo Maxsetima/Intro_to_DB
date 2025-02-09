9b5a2cc481df0e7b335f625ec79de611717e1b3c
Maxsetima <kipsangk98@gmail.com>
2025-02-09 22:04:48 +0300

    Ensure correct insert of customers with exact address values

--- task_6.sql
+++ task_6.sql
@@ -4,6 +4,6 @@ USE alx_book_store;
 -- Insert multiple rows into the customers table
 INSERT INTO customers (customer_name, email, address)
 VALUES
-    ('Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    ('Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
     ('Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
     ('Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
