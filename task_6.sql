f09dd4dbf1df181c310cd746fcac8ba5111e9e2f
Maxsetima <kipsangk98@gmail.com>
2025-02-09 20:42:12 +0300

    update changes

--- task_6.sql
+++ task_6.sql
@@ -1,16 +1,8 @@
 -- Use the alx_book_store database
 USE alx_book_store;
 
--- Insert multiple rows into the customer table
-UPDATE customers
-SET customer_name = 'Blessing Malik', email = 'bmalik@sandtech.com', address = '124 Happiness Ave.'
-WHERE customer_id = 2;
-
-UPDATE customers
-SET customer_name = 'Obed Ehoneah', email = 'eobed@sandtech.com', address = '125 Happiness Ave.'
-WHERE customer_id = 3;
-
-UPDATE customers
-SET customer_name = 'Nehemial Kamolu', email = 'nkamolu@sandtech.com', address = '126 Happiness Ave.'
-WHERE customer_id = 4;
-
+INSERT INTO customers (customer_name, email, address)
+VALUES
+    ('Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    ('Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+    ('Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
