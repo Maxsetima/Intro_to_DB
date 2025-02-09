fe86650acee7d64db05ff5e7da17fd4f0de9bde1
Maxsetima <kipsangk98@gmail.com>
2025-02-09 18:35:32 +0300

    Add task_3.sql to list all tables in alx_book_store

---
+++ task_3.sql
@@ -0,0 +1,6 @@
+-- task_3.sql: Script to list all tables in alx_book_store
+
+-- Connect to MySQL and pass the database name as an argument
+-- The database name should be passed as a command-line argument
+
+SHOW TABLES;
